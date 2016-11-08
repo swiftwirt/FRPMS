9e1879fbc9ef5289c645128f37d110b22e7970cf
twof <fabiobean2@gmail.com>
2016-11-08 11:27:43 -0800

    Data bindings demo passing

--- 1108-tu/DataBindings-demo/DataBindings-demo/ViewController.swift
+++ 1108-tu/DataBindings-demo/DataBindings-demo/ViewController.swift
@@ -11,54 +11,68 @@ import RxSwift
 import RxCocoa
 
 enum DataToUpdate: String {
-  case name = "Name"
-  case age = "Age"
+    case name = "Name"
+    case age = "Age"
 }
 
 class ViewController: UIViewController {
-  
-  var name = "Johnny"
-  var age = 17
-  
-  @IBOutlet weak var nameLabel: UILabel!
-  @IBOutlet weak var ageLabel: UILabel!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-  
-  @IBAction func changeNameButtonPressed(_ sender: AnyObject) {
-    showAlertController(dataToUpdate: .name)
-  }
-  
-  @IBAction func changeAgeButtonPressed(_ sender: AnyObject) {
-    showAlertController(dataToUpdate: .age)
-  }
-  
-  func showAlertController(dataToUpdate: DataToUpdate) {
-    let title = "Update \(dataToUpdate.rawValue)"
-    let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
-    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-    alert.addAction(cancelAction)
-    let okAction = UIAlertAction(title: "OK", style: .default) { action in
-      if let newValue = alert.textFields?.first?.text {
-        switch dataToUpdate {
-        case .age:
-          self.age = Int(newValue)!
-        case .name:
-          self.name = newValue
+    
+    var disposeBag = DisposeBag()
+    
+    var name: Variable<String> = Variable("Johnny")
+    var age: Variable<Int> = Variable(17)
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        age
+            .asObservable()
+            .map{ String($0) }
+            .bindTo(ageLabel.rx.text)
+            .addDisposableTo(disposeBag)
+        
+        name
+            .asObservable()
+            .bindTo(nameLabel.rx.text)
+            .addDisposableTo(disposeBag)
+    }
+    
+    @IBAction func changeNameButtonPressed(_ sender: AnyObject) {
+        showAlertController(dataToUpdate: .name)
+    }
+    
+    @IBAction func changeAgeButtonPressed(_ sender: AnyObject) {
+        showAlertController(dataToUpdate: .age)
+    }
+    
+    func showAlertController(dataToUpdate: DataToUpdate) {
+        let title = "Update \(dataToUpdate.rawValue)"
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(cancelAction)
+        let okAction = UIAlertAction(title: "OK", style: .default) { action in
+            if let newValue = alert.textFields?.first?.text {
+                switch dataToUpdate {
+                case .age:
+                    self.age.value = Int(newValue)!
+                case .name:
+                    self.name.value = newValue
+                }
+            }
         }
-      }
+        alert.addAction(okAction)
+        alert.addTextField() { textField in
+            switch dataToUpdate {
+            case .age: textField.keyboardType = .numberPad
+            default: break
+            }
+        }
+        present(alert, animated: true)
     }
-    alert.addAction(okAction)
-    alert.addTextField() { textField in
-      switch dataToUpdate {
-      case .age: textField.keyboardType = .numberPad
-      default: break
-      }
-    }
-    present(alert, animated: true)
-  }
-  
+    
 }
 
